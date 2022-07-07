d9d2f751cee08b96a641eecb8a59a3f71ee7cda7
JAzerad <62903133+JAzerad@users.noreply.github.com>
2022-07-07 00:29:28 -0400

    Update 2022.js

--- src/js/data/2022.js
+++ src/js/data/2022.js
@@ -26,5 +26,293 @@ dataSet[dataSetVersion].characterData = [
       series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
       stage: ["st4", "ex"]
     }
-  }
+  },
+  {
+    name: "Kallen",
+    img: "LSt4VJP.jpg",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Serena",
+    img: "TaV7dd0.jpg",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Akane",
+    img: "fhJQCkY.jpg",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Ako (KissxSis)",
+    img: "TN8H8s7.jpeg",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Ako (NetoGe)",
+    img: "aHtpnXg.jpg",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Amy",
+    img: "BUngwLP.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Ako (NetoGe)",
+    img: "aHtpnXg.jpg",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Anna",
+    img: "wLgQOe3.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Echidna",
+    img: "gdJmxOn.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+    {
+    name: "Eris",
+    img: "2GeONd8.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+    {
+    name: "Ghislaine",
+    img: "Vrtk4mf.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+    {
+    name: "Hibiki",
+    img: "kpXQ7UE.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+    {
+    name: "Hibiki Sakura",
+    img: "gdJmxOn.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+    {
+    name: "Ikaros",
+    img: "UHY4bIG.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Illya",
+    img: "HypUlY5.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },  
+  {
+    name: "Kobeni",
+    img: "PwwJPup.jpg",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  }, 
+  {
+    name: "Kongou",
+    img: "aPTs3Vn.jpg",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  }, 
+  {
+    name: "Mikoto",
+    img: "IcELV7v.jpg",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Mao",
+    img: "csXd4LA.jpg",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Medaka",
+    img: "d3baGKP.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  }, 
+  {
+    name: "Miki Hoshi",
+    img: "2NXc5pQ.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Kanroji",
+    img: "2NXc5pQ.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Hibiki Sakura",
+    img: "gdJmxOn.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Nobara",
+    img: "QQgGgqv.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Palutena",
+    img: "tnDzpx2.jpg",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Akane",
+    img: "tQL3QXW.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Sakura Matou",
+    img: "TeaPvLv.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Saya Takagi",
+    img: "6yABxC1.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Stella Vermillion",
+    img: "I5JE76n.jpg",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "Yae Miko",
+    img: "ws2bSuA.jpg",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+    {
+    name: "Yourichi",
+    img: "vVLZfWg.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+      {
+    name: "Utaha",
+    img: "OP5J1Jg.jpg",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+      {
+    name: "Hinata",
+    img: "DNOk77L.jpg",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+      {
+    name: "Megumi",
+    img: "T2jexqN.jpg",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+      {
+    name: "Nejire",
+    img: "tUma8mw.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
+      {
+    name: "Hatsume",
+    img: "agwaQ7O.png",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "MoF", "SA", "UFO", "soku", "DS", "GFW", "TD", "HM", "DDC", "ISC", "ULiL", "LoLK", "AoCF", "HSiFS", "VD", "WBaWC", "UM" ],
+      stage: ["st4", "ex"]
+    }
+  },
 ];
